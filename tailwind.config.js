e7a832a2e9d4ea1a52c3b55a72080369541c899a
JeromeL00 <0319-2584@lspu.edu.ph>
2023-03-31 00:08:01 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#51E9EF',
+        secondary: '#39B0A8',
+        dark: '#1C1C1C',
+        Darkgray: '#5C5B5B',
+        lightGray: '#E5E5E5',
+      },
+      fontFamily : {
+        poppins : ['Poppins', 'sans-serif']
+      },
+      fontSize: {
+        huge: 'clamp(3rem, 6vw, 5rem)',
+        norm: 'clamp(1rem, 6vw, 2rem)',
+      },
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1248px',
+        /*'2xl' : '1496px',*/
+      },
+    },
+  },
+  plugins: [],
+}
+
